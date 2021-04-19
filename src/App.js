ef7ae837962e7ead35760c30e26fdf61a88ca990
shinji0320 <shinji.blue.one@gmail.com>
2021-04-20 08:34:57 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Field from "./components/Field";
+import Button from "./components/Button";
+import ManipulationPanel from "./components/ManipulationPanel";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="title-container">
+          <h1 className="title">Snake Game</h1>
+        </div>
+        <Navigation />
+      </header>
+      <main className="main">
+        <Field />
+      </main>
+      <footer className="footer">
+        <Button />
+        <ManipulationPanel />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
