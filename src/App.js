f8a0e16f279af6f52af59897288d4695c0afa755
shinji0320 <shinji.blue.one@gmail.com>
2021-04-24 10:55:25 +0900

    [GameOver]EatingMyself

--- src/App.js
+++ src/App.js
@@ -65,6 +65,10 @@ const isCollision = (fieldSize, position) => {
   return false;
 };
 
+const isEatingMyself = (fields, position) => {
+  return fields[position.y][position.x] === 'snake'
+}
+
 function App() {
   const [fields, setFields] = useState(initialValues);
   const [body, setBody] = useState([]);
@@ -138,7 +142,7 @@ function App() {
       y: y + delta.y,
     };
 
-    if (isCollision(fields.length, newPosition)) {
+    if (isCollision(fields.length, newPosition) || isEatingMyself(fields, newPosition)) {
       return false;
     }
     const newBody = [...body];
