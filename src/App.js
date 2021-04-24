72e3ab0d28f4bb27344f62e25e6fa83bf12ff4ba
shinji0320 <shinji.blue.one@gmail.com>
2021-04-24 12:30:00 +0900

    難易度変更可能に

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { initFields, getFoodPosition } from "./utils/index";
 const initialPosition = { x: 17, y: 17 };
 const initialValues = initFields(35, initialPosition);
 const defaultInterval = 100;
+const defaultDifficulty = 3
+
+const Difficulty = [1000, 500, 100, 50, 10]
 
 const GameStatus = Object.freeze({
   init: "init",
@@ -74,16 +77,18 @@ function App() {
   const [body, setBody] = useState([]);
   const [status, setStatus] = useState(GameStatus.init);
   const [direction, setDirection] = useState(Direction.up);
+  const [difficulty, setDifficulty] = useState(defaultDifficulty);
   const [tick, setTick] = useState(0);
 
   useEffect(() => {
     setBody([initialPosition]);
     //ゲームの中の時間を管理する
+    const interval = Difficulty[difficulty -1]
     timer = setInterval(() => {
       setTick((tick) => tick + 1);
-    }, defaultInterval);
+    }, interval);
     return unsubscribe;
-  }, []);
+  }, [difficulty]);
 
   useEffect(() => {
     if (body.length === 0 || status !== GameStatus.playing) {
@@ -117,6 +122,19 @@ function App() {
       setDirection(newDirection);
     },
     [direction, status]
+  );
+
+  const onChangeDifficulty = useCallback(
+    (difficulty) => {
+      if (status !== GameStatus.init) {
+        return;
+      }
+      if (difficulty < 1 || difficulty > Difficulty.length) {
+        return;
+      }
+      setDifficulty(difficulty);
+    },
+    [status, difficulty]
   );
 
   useEffect(() => {
@@ -172,7 +190,11 @@ function App() {
         <div className="title-container">
           <h1 className="title">Snake Game</h1>
         </div>
-        <Navigation />
+        <Navigation
+          length={body.length}
+          difficulty={difficulty}
+          onChangeDifficulty={onChangeDifficulty}
+        />
       </header>
       <main className="main">
         <Field fields={fields} />
